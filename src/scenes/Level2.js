f605dd44e4910582305a50c0bcb13b3e4990342b
aplaxco1 <aplaxco@ucsc.edu>
2022-05-21 17:16:54 -0700

    addin the map to lvl 2

--- src/scenes/Level2.js
+++ src/scenes/Level2.js
@@ -7,46 +7,43 @@ class Level2 extends Phaser.Scene {
     }
 
     create() {
+
         console.log("it lvl 2");
-        // set world and camera bounds
-        this.physics.world.setBounds(0, 0, game.config.width*2, game.config.height*5);
+        // set world and camera bounds (the world bounds are greater than the camera so that the player can move off to
+        // the right end of the screen to progress to level 3)
+        this.physics.world.setBounds(0, 0, game.config.width*3, game.config.height*5);
         this.cameras.main.setBounds(0, 0, game.config.width*2, game.config.height*5);
+        // setting the background color
+        this.cameras.main.setBackgroundColor('#808080');
 
         // level 2 music
         // this.music = this.sound.add('lvl1_music', { loop: true, volume: 0.5 });
         // this.music.play();
 
-        // setting the background color
-        this.cameras.main.setBackgroundColor('#808080');
-
-        //this.background = this.add.sprite(0, 0, 'lvl1_background').setOrigin(0, 0);
-        //this.background.setScrollFactor(0);
-
         // addin RoboCat to the scene and make it so they can't go OoB
-        this.cat = new RoboCat(this, 10, game.config.height - 44, 'robo_atlas', 'robo_idle_r_0001').setOrigin(0,0);
+        this.cat = new RoboCat(this, 10, game.config.height*5 - 44, 'robo_atlas', 'robo_idle_r_0001').setOrigin(0,0);
         this.cat.setCollideWorldBounds(true);
         //makes it so the cat goes in front of controls text
         this.cat.setDepth(1);
 
-        //cat has propeller for lvl 2
-        hasPropeller = true;
-
-        // this SHOULD make it so that the camera follows RoboCat as the player moves (this wont do the whole room switching thing, but
-        // we can add that later, this is just temporary)
+        // makes it so that the camera follows RoboCat as the player moves
         this.cameras.main.startFollow(this.cat);
 
-        // this sets up the ground (again, this will change when the obstacles are implemented and we have an actual tile
-        // sheet to pull these from)
-        this.ground = this.add.group();
-        this.tileSize = 21;
-        for(let i = 0; i < (game.config.width*2 + game.config.width/4); i += this.tileSize) {
-            let groundTile = this.physics.add.sprite(i, game.config.height - this.tileSize, 'platform_tile', 0).setOrigin(0);
-            groundTile.body.immovable = true;
-            groundTile.body.allowGravity = false;
-            this.ground.add(groundTile);
-        }
-        // sets collider between RoboCat and the ground
-        this.physics.add.collider(this.cat, this.ground);
+        const map = this.make.tilemap({ key: 'lvl2_tilemap' });
+        const tileset = map.addTilesetImage('lvl2_tiles', 'lvl2_tiles');
+        // creating tilemap layers
+        this.groundLayer = map.createStaticLayer('Ground', tileset, 0, 0);
+        this.spikesLayer = map.createStaticLayer('Spikes', tileset, 0, 0);
+        this.leavesLayer = map.createStaticLayer('Leaves', tileset, 0, 0);
+        // enabling collisons on tilemap layers
+        this.groundLayer.setCollisionByExclusion([-1]);
+        this.spikesLayer.setCollisionByExclusion([-1]);
+        // enabling collisions with player
+        this.physics.add.collider(this.cat, this.groundLayer);
+        this.physics.add.collider(this.cat, this.spikesLayer, function(player) {
+            player.setVelocity(0);
+            player.resetPosition(10, game.config.height*5 - 60, false);
+        });
 
         //key inputs
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -55,16 +52,7 @@ class Level2 extends Phaser.Scene {
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     }
 
-
     update() {
-         // check if player falls through floor, telports them back to begining if they do
-         if (this.cat.y > (game.config.height + this.cat.height)) {
-            this.cat.setVelocity(0);
-            this.cat.x = 10;
-            this.cat.y = game.config.height - 60;
-        } 
-
-        // update cat sprite
         this.cat.update();
     }
 }

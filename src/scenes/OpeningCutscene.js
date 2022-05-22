962f5ca912844aa4447d8d6a05c69e32778f736e
Elizabeth Arnold <eroxarnmc@gmail.com>
2022-05-21 18:37:29 -0700

    made the cat run off a cliff and die

--- src/scenes/OpeningCutscene.js
+++ src/scenes/OpeningCutscene.js
@@ -14,37 +14,51 @@ class OpeningCutscene extends Phaser.Scene {
         this.cameras.main.setBackgroundColor('#2d2d2d');
 
         this.catFallPath = new Phaser.Curves.Path(100,100);
-        this.catFallPath.lineTo(200,100);
-        this.catFallPath.lineTo(200, 250);
+        this.catFallPath.lineTo(200, 100);
+        this.catFallPath.lineTo(200, 200);
         this.cat = this.add.follower(this.catFallPath, 100, 100, 'robo_atlas', 'robo_idle_r_0001');
-        this.cat.anims.play('robo_run_r');
-        this.cat.startFollow({
+
+        this.catTweenConfig = {
+            targets: 'catFallPath',
+            props: {},
             from: 0,
             to: 1,
             delay: 0,
-            duration: 2000,
-            ease: 'linear',
+            duration: this.duration,
+            ease: "linear",
             hold: 0,
             yoyo: false,
             rotateToPath: true,
-            onCompleteScope: this,
-            onComplete: this.explode
-        }); 
+            callbackScope: this,
+           onStart: function() {
+                this.cat.anims.play('robo_run_r');
+            },
+            onUpdate: function() {
+                console.log(this.cat.angle);
+                if(this.cat.angle == 90 && this.cat.anims.isPlaying 
+                    && this.cat.anims.currentAnim.key == 'robo_run_r') {
+                    this.cat.anims.play('robo_jump_r');
+                }
+            },
+            onComplete: function() {
+                this.cat.anims.play('robo_explosion');
+                this.cat.on('animationcomplete', () => {
+                    this.cat.destroy();
+                });
+            }
+        } 
+
+        this.cat.startFollow(this.catTweenConfig);
+        
 
 
         //transition to level 1 at the end of the cutscene
-      /*  this.time.delayedCall(this.duration, () => {
+        this.time.delayedCall(this.duration + 1000, () => {
             this.scene.start('Level1Scene');
-        });*/
+        });
     }
 
     update() {
 
-    }
-    explode() {
-        this.cat.anims.play('robo_explosion');
-        this.cat.on('animationcomplete', () => {
-            this.cat.destroy();
-        })
     }
 }

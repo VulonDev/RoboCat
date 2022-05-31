c6f6fce04b2bf530226c135565656d49641ffae0
Elizabeth Arnold <eroxarnmc@gmail.com>
2022-05-31 01:37:03 -0700

    upgraded cutscene

--- src/scenes/OpeningCutscene.js
+++ src/scenes/OpeningCutscene.js
@@ -9,6 +9,9 @@ class OpeningCutscene extends Phaser.Scene {
         this.load.path = 'assets/';
         this.load.image('cat', 'cutscene/cat.png' );
         this.load.image('fallbg', 'cutscene/cutscene roof.png');
+        this.load.image('lightning', 'cutscene/cutscene lightning.png');
+        //load spritesheet
+        this.load.spritesheet('rain', 'cutscene/cutscene rain.png', {frameWidth: 420, frameHeight: 294, startFrame: 0, endFrame: 1});
     }
     
     create() {
@@ -17,12 +20,27 @@ class OpeningCutscene extends Phaser.Scene {
         // setting level background image
         this.background = this.add.sprite(0, 0, 'fallbg').setOrigin(0, 0);
         this.background.setScrollFactor(0);
+        //rain
+        this.anims.create({
+            key: 'rainanim',
+            frames: this.anims.generateFrameNumbers('rain', {start: 0, end: 1, first:0}),
+            frameRate: 15,
+            repeat: -1
+        });
+        this.rain = this.add.sprite(0,0, 'rain').setOrigin(0,0);
+        this.rain.anims.play('rainanim');
+
+        //lightning
+        this.lightning = this.add.sprite(0,0, 'lightning').setOrigin(0,0);
+        this.lightning.setAlpha(0);
+
 
         this.catFallPath = new Phaser.Curves.Path(0,40);
         this.catFallPath.lineTo(230, 40);
         this.catFallPath.splineTo([290, 180]);
         this.catFallPath.lineTo(290, 275);
         this.cat = this.add.follower(this.catFallPath, 50, 40, 'robo_atlas', 'robo_idle_r_0001');
+        this.lightningplayed = false;
 
         this.catTweenConfig = {
             targets: 'catFallPath',
@@ -43,6 +61,13 @@ class OpeningCutscene extends Phaser.Scene {
                 if(this.cat.x >= 190 && this.cat.anims.currentAnim.key == 'robo_run_r') {
                     this.cat.anims.play('robo_jump_r');
                 }
+                if(this.cat.x >=265 && this.cat.x < 280 && !this.lightningplayed) {
+                    this.lightningplayed = true;
+                    this.cameras.main.flash(175);
+                }
+                if(this.cat.x >= 283 && this.lightningplayed) {
+                    this.lightningplayed = false;
+                }
             },
             onComplete: function() {
                 this.cat.anims.play('robo_explosion');
@@ -58,13 +83,18 @@ class OpeningCutscene extends Phaser.Scene {
 
 
         //transition to level 1 at the end of the cutscene
-        this.time.delayedCall(this.duration + 1000, () => {
+       /* this.time.delayedCall(this.duration + 1000, () => {
             this.scene.stop();
             this.scene.start('Level1Scene');
-        });
+        });*/
     }
 
     update() {
-
+        if(this.lightningplayed) {
+            this.lightning.setAlpha(1);
+        }
+        else{
+            this.lightning.setAlpha(0);
+        }
     }
 }
